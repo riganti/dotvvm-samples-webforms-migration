952a8f890c3a40e42ac8a6c740d681bc03857249
Tomáš Herceg <herceg@vbnet.cz>
2019-05-07 18:00:03 +0200

    DotVVM installed

---
+++ src/OldWebApp/OldWebApp/DotvvmStartup.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using DotVVM.Framework.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace OldWebApp
+{
+    public class DotvvmStartup : IDotvvmStartup, IDotvvmServiceConfigurator
+    {
+        public void Configure(DotvvmConfiguration config, string applicationPath)
+        {
+            ConfigureRoutes(config, applicationPath);
+            ConfigureControls(config, applicationPath);
+            ConfigureResources(config, applicationPath);
+        }
+
+        private void ConfigureRoutes(DotvvmConfiguration config, string applicationPath)
+        {
+        }
+
+        private void ConfigureControls(DotvvmConfiguration config, string applicationPath)
+        {
+        }
+
+        private void ConfigureResources(DotvvmConfiguration config, string applicationPath)
+        {
+        }
+
+        public void ConfigureServices(IDotvvmServiceCollection options)
+        {
+            options.AddDefaultTempStorages("Temp");
+        }
+    }
+}
---
+++ src/OldWebApp/OldWebApp/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web.Hosting;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(OldWebApp.Startup))]
+
+namespace OldWebApp
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseDotVVM<DotvvmStartup>(HostingEnvironment.ApplicationPhysicalPath);
+        }
+    }
+}
