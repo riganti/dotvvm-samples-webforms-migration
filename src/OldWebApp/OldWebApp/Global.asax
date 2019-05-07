7e88ec44e6dcb2ae6070fddb0dc1ce7c7b44560c
Tomáš Herceg <herceg@vbnet.cz>
2019-05-07 18:28:44 +0200

    Project switched to .NET Core

--- src/OldWebApp/OldWebApp/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OldWebApp.Global" Language="C#" %>
--- src/OldWebApp/OldWebApp/Startup.cs
+++ src/OldWebApp/OldWebApp/Startup.cs
@@ -1,18 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
-using System.Web.Hosting;
-using Microsoft.Owin;
-using Owin;
-
-[assembly: OwinStartup(typeof(OldWebApp.Startup))]
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace OldWebApp
 {
     public class Startup
     {
-        public void Configuration(IAppBuilder app)
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
         {
-            app.UseDotVVM<DotvvmStartup>(HostingEnvironment.ApplicationPhysicalPath);
+            services.AddAuthorization();
+            services.AddDataProtection();
+
+            services.AddDotVVM<DotvvmStartup>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseDotVVM<DotvvmStartup>();
+
+            app.UseStaticFiles();
         }
     }
 }
