ccb979d6ad915f5438a827149ef206078815efc8
Tomáš Herceg <tomas.herceg@riganti.cz>
2019-09-21 19:16:44 +0200

    Entity Framework added Contact page removed

--- src/OldWebApp/OldWebApp/Global.asax.cs
+++ src/OldWebApp/OldWebApp/Global.asax.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Routing;
 using System.Web.Security;
 using System.Web.SessionState;
+using OldWebApp.Model;
 
 namespace OldWebApp
 {
@@ -12,9 +14,10 @@ namespace OldWebApp
     {
         void Application_Start(object sender, EventArgs e)
         {
+            Database.SetInitializer(new AppDbInitializer());
+
             RouteTable.Routes.MapPageRoute("Default", "", "~/Default.aspx");
             RouteTable.Routes.MapPageRoute("About", "About", "~/About.aspx");
-            RouteTable.Routes.MapPageRoute("Contact", "Contact", "~/Contact.aspx");
         }
     }
 }
